e83c82315a031a83998178cf3990e5fcf20c1d8e
sirichad-kat <sirichad.k@gmail.com>
2026-05-13 12:44:56 +0700

    Create CourseMapper.cs

---
+++ src/DotnetEFProject/DotnetEFProject.Infrastructure/Mappings/CourseEnrollment/CourseMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetEFProject.Infrastructure.Mappings.CourseEnrollment
+{
+    public static class CourseMapper
+    {
+    }
+}
